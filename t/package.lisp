f3920b9df19cb7bd7d44eb3c5cbdebea06c743e0
Masataro Asai <guicho2.71828@gmail.com>
2019-01-08 23:07:53 +0900

    test no-debug

--- t/package.lisp
+++ t/package.lisp
@@ -20,11 +20,19 @@
 ;; run test with (run! test-name) 
 
 (test cl-sat.minisat
+  (is-true (nth-value 1 (solve '(and a b c) :minisat :debug t)))
+  (is-true (nth-value 1 (solve '(or a b c) :minisat :debug t)))
+  (is-true (nth-value 1 (solve '(and (or a !b c) d) :minisat :debug t)))
+  (is-true (nth-value 1 (solve '(and (and (and a))) :minisat :debug t)))
+  (is-true (nth-value 1 (solve '(not (and a b)) :minisat :debug t)))
+  (is-true (nth-value 1 (solve '(not (or a b)) :minisat :debug t)))
+  (is-false (nth-value 1 (solve '(and a !a) :minisat :debug t))))
+
+(test cl-sat.minisat.no-debug
   (is-true (nth-value 1 (solve '(and a b c) :minisat)))
   (is-true (nth-value 1 (solve '(or a b c) :minisat)))
   (is-true (nth-value 1 (solve '(and (or a !b c) d) :minisat)))
   (is-true (nth-value 1 (solve '(and (and (and a))) :minisat)))
   (is-true (nth-value 1 (solve '(not (and a b)) :minisat)))
   (is-true (nth-value 1 (solve '(not (or a b)) :minisat)))
-  (is-false (nth-value 1 (solve '(and a !a) :minisat)))
-  (is-true (nth-value 1 (solve '(not (or a b)) :minisat "-cpu-lim=10000"))))
+  (is-false (nth-value 1 (solve '(and a !a) :minisat))))
